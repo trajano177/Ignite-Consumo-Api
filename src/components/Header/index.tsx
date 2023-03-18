752b6497cd5455a13ae7127379bf598e4e1cb457
matheus trajano <matheustrajanotel@gmail.com>
2023-03-17 22:51:10 -0300

    att

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoSVG from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "./../NewTransactionModal/index";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoSVG} />
-        <NewTransactionButton> Nova transaçâo</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton> Nova transaçâo</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
 }
+
+// dialog root em volta de todo o modal, o trigger para abri o modal, o portal para poder abrir o modal em qualquer outro lugar da aplicação
+// overlay para criar um fundo preto e o content é todo conteúdo do modal
